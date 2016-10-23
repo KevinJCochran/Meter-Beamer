68623ef1e3007c75ce3478b4ca022de6df4576e8
Kevin <kevinjcochran@gmail.com>
2016-10-23 03:55:43 -0400

    add php

---
+++ con.php
@@ -0,0 +1,34 @@
+<?php
+	
+	// read in from html
+	if($_POST["submit"]){
+
+		$firstName = $_POST["first_name"];
+		$lastName = $_POST["last_name"];
+		$address = $_POST["address"];
+		$userName = $_POST["user_name"];
+		$password = $_POST["password"];
+
+		// connect to mongodb
+		$m = new MongoClient();
+		echo "Connection to database successfully";
+		// select a database
+		$db = $m->parkdb;
+		echo "Database parkdb selected";
+
+		// select collection
+		$collection = $db->users;
+		echo "Collection selected succsessfully";
+
+		$document = array( 
+	      	"firstName" => $firstName, 
+	      	"lastName" => $lastName, 
+	      	"address" => $address,
+	      	"userName" => $userName,
+	      	"password" => $password
+		);
+	
+		$collection->insert($document);
+		echo "Document inserted successfully";
+	}
+?>
