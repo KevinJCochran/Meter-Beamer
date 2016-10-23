05ae2a9e0b9bdeb2b65645dbf3ada63684ce041e
owner <owner@nwk--192-192.nwk.ruw.rutgers.edu>
2016-10-22 23:47:21 -0400

    add js

---
+++ capitalone.js
@@ -0,0 +1,7 @@
+$.ajax({
+    url: 'http://api.reimaginebanking.com/merchants/580c1abe360f81f104544f09?key=bb67d82aa016521f06cd766920159874',
+    success: function(results)
+    {
+    	    console.log("HEY WE HAVE A MERCHANT!!!!") 
+    }
+});
